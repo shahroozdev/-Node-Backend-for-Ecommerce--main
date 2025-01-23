960e900f16c4d202b081dfe2e45c18bd33234625
shahroozdev <shahroozdeveloper@gmail.com>
2025-01-23 11:49:48 +0500

    some updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,10 +13,7 @@ const connectToDatabase = async () => {
 
   try {
     // Connect to the database
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(process.env.MONGO_URI);
 
     isConnected = db.connections[0].readyState; // Set connection status
     console.log("Connected to MongoDB");
